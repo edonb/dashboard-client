0ef88d070d2918731a88a6ad06ad82205992fd62
edonb <110935690+edonb@users.noreply.github.com>
2023-05-11 23:50:25 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   const [usdNok, setUsdNok] = useState(null);
   const [eurNok, setEurNok] = useState(null);
 
-  const serverUrl = "http://192.168.0.162:5000";
+  const serverUrl = "http://192.168.1.165:5000";
   const cryptoApiUrl = `${serverUrl}/crypto`;
   const exchangeApiUrl = `${serverUrl}/exchange`;
 
